57a018a9347aa92ebf78d56c273cefde6bfdfcbf
Crowdbotics <team@crowdbotics.com>
2024-03-13 19:46:55 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aged-shape-47014",
+  url: "https://aged-shape-47014.botics.co",
+  api: "https://aged-shape-47014.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
